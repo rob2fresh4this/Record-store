a315d0593fd5c784918567d9dc5f62107e6f7ed2
rob2fresh4this <grijrob07@gmail.com>
2023-11-16 10:52:19 -0800

    chages to nav bar & clean up, chages to gallery js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 //NAV BAR//
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-
-function myFunction() {
-  let sticky = document.body.offsetWidth * .20
-  console.log(sticky)
-  console.log(window.pageYOffset)
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 //🗿🗿🗿
 
@@ -20,17 +7,47 @@ function myFunction() {
 
 //GALLERY SLIDER//
 
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 5000); // Change image every # seconds
+// }
+
 var myIndex = 0;
+var isPaused = false;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000); // Change image every # seconds
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1;
+    }    
+    x[myIndex-1].style.display = "block";  
+
+    if (!isPaused) {
+        setTimeout(carousel, 5000); // Change image every 5 seconds
+    }
+}
+
+function pauseCarousel() {
+    isPaused = true;
+}
+
+function resumeCarousel() {
+    isPaused = false;
+    carousel(); // Start the carousel again when mouse leaves
 }
