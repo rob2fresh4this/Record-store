1a9cf57face20773bca1dc98cd881ec6cf6b3f8f
rob2fresh4this <grijrob07@gmail.com>
2023-11-16 10:52:40 -0800

    update

--- script.js
+++ script.js
@@ -5,22 +5,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-//GALLERY SLIDER//
-
-// var myIndex = 0;
-// carousel();
-
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {myIndex = 1}    
-//   x[myIndex-1].style.display = "block";  
-//   setTimeout(carousel, 5000); // Change image every # seconds
-// }
 
 var myIndex = 0;
 var isPaused = false;
